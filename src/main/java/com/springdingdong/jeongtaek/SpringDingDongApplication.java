800e927eb55ea5754e284788ea82bc38460e1d9b
JeongTaek <jtkim965@naver.com>
2025-01-08 17:31:06 +0900

    Git Init
    qwe

---
+++ src/main/java/com/springdingdong/jeongtaek/SpringDingDongApplication.java
@@ -0,0 +1,13 @@
+package com.springdingdong.jeongtaek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDingDongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDingDongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springdingdong/jeongtaek/SpringDingDongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdingdong.jeongtaek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDingDongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
